32f2ce194fa5135665eefd5c955a1cce0daf92bf
jekafortuna <evgeniy.fortuna@gmail.com>
2016-12-24 23:51:22 +0200

    Different patterns tasks

---
+++ src/main/java/ua/kpi/PatternsTasks/HumanRolesUnion/Location.java
@@ -0,0 +1,7 @@
+package ua.kpi.PatternsTasks.HumanRolesUnion;
+
+/**
+ * Created by Evgeniy on 24.12.2016.
+ */
+public class Location {
+}
---
+++ src/main/java/ua/kpi/PatternsTasks/Main.java
@@ -0,0 +1,7 @@
+package ua.kpi.PatternsTasks;
+
+/**
+ * Created by Evgeniy on 24.12.2016.
+ */
+public class Main {
+}
