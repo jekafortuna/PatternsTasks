b95d4ddda06b6a35d595431670ad8cf7129b2347
jekafortuna <evgeniy.fortuna@gmail.com>
2016-12-25 23:58:56 +0200

    Different patterns tasks

--- src/main/java/ua/kpi/PatternsTasks/HumanRolesUnion/Human.java
+++ src/main/java/ua/kpi/PatternsTasks/HumanRolesUnion/Human.java
@@ -6,6 +6,7 @@ package ua.kpi.PatternsTasks.HumanRolesUnion;
 public class Human {
     private Location location;
     private HumanState humanState;
+    private Human nextHumanState;
 
     public Location getLocation() {
         return location;
@@ -13,5 +14,34 @@ public class Human {
 
     public void setLocation(Location location) {
         this.location = location;
+    }
+
+    public void setStateByLocation(){
+        if (this.getLocation() == Location.BEACH_OF_RIVER){
+            System.out.println("We are at the beach of river! ");
+            this.humanState = new Fisher();
+        }
+        else if (this.getLocation() == Location.LAND_OF_MUSHROOMS){
+            System.out.println("We are at the land of mushrooms! ");
+            this.humanState = new Mushroomer();
+        }
+        else if (this.getLocation() == Location.MEET_AN_ANIMAL){
+            System.out.println("We meet an animal! Be careful! ");
+            this.humanState = new Hunter();
+        }
+        else {
+            System.out.println("No such state!!! ");
+        }
+    }
+
+    public void setNextState(Human location){
+        nextHumanState = location;
+    }
+
+    public void changeRole(){
+        humanState.changeRole();
+        if (nextHumanState != null){
+            nextHumanState.changeRole();
+        }
     }
 }
--- src/main/java/ua/kpi/PatternsTasks/HumanRolesUnion/Main.java
+++ src/main/java/ua/kpi/PatternsTasks/HumanRolesUnion/Main.java
@@ -4,4 +4,19 @@ package ua.kpi.PatternsTasks.HumanRolesUnion;
  * Created by Evgeniy on 24.12.2016.
  */
 public class Main {
+    public static void main(String[] args) {
+        Human human = new Human();
+
+        human.setLocation(Location.LAND_OF_MUSHROOMS);
+        human.setStateByLocation();
+        human.changeRole();
+
+        Human human2 = new Human();
+        human2.setLocation(Location.MEET_AN_ANIMAL);
+        human2.setStateByLocation();
+//        human2.changeRole();
+
+        human.setNextState(human2);
+        human.changeRole();
+    }
 }
