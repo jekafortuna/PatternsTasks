eeb6f3261306be9c2c84a5c43050f578004fc282
jekafortuna <evgeniy.fortuna@gmail.com>
2016-12-29 01:08:19 +0200

    Salad from ingredients

--- src/main/java/ua/kpi/PatternsTasks/SaladFromIngredients/Main.java
+++ src/main/java/ua/kpi/PatternsTasks/SaladFromIngredients/Main.java
@@ -19,5 +19,19 @@ public class Main {
                       .addIngredient(Ingredient.PEPER, 6)
                       .addIngredient(Ingredient.SALT, 2);
 
+        Salad salad = new Salad.Builder()
+                .setIngredientsAvailable(ingredientsBox.getIngredients())
+                .addIngredient(Ingredient.TOMATO)
+                .addIngredient(Ingredient.CUCUMBER)
+                .addIngredient(Ingredient.ONION)
+                .addIngredient(Ingredient.SALAD_LEAVES)
+                .addIngredient(Ingredient.SALAD_LEAVES)
+                .addIngredient(Ingredient.SALT)
+                .addIngredient(Ingredient.CHEESE)
+                .addIngredient(Ingredient.PARSLEY)
+                .build();
+
+        ingredientsBox.setIngredients(salad.getAvailableIngredients());
+        salad.showSalad();
     }
 }
